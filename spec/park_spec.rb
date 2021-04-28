1d795a6ee8dfe9fa76b82843d8b32aebc2251a01
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-28 09:14:46 -0600

    Green creat and red intitialize for Park

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe 'Park' do
 
       expect(park1.class).to eq(Park)
     end
+    it 'has all initial attributes' do
+      park1 = Park.new('Capitol Reef')
+
+      expect(park1.name).to eq('Capitol Reef')
+      expect(park1.trails).to eq([])
+    end
   end
 end
