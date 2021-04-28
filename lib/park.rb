0e58664a71b150e1b2d9bdf755dad0ace217179c
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-28 09:18:08 -0600

    Green intialize and red add trials for Park

--- lib/park.rb
+++ lib/park.rb
@@ -1,4 +1,9 @@
 class Park
+  attr_reader :name,
+              :trails
+
   def initialize(name)
+    @name = name
+    @trials = []
   end
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe 'Park' do
       expect(park1.trails).to eq([])
     end
   end
+  describe '#add_trail' do
+    it 'adds a trail instace to the trails array' do
+      park1 = Park.new('Capitol Reef')
+      trail1 = Trail.new(name: 'Grand Wash', length: '2.2 miles', level: :easy)
+      trail2 = Trail.new(name: 'Cohab Canyon', length: '1.7 miles', level: :moderate)
+
+      park1.add_trail(trail1)
+
+      expect(park1.trails).to eq([trail1])
+
+      park1.add_trail(trail2)
+
+      expect(park1.trails).to eq([trail1, trial2])
+    end
+  end
 end
