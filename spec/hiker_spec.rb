f46c41de2f4908ef4028a10240046f5d43232b15
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-28 09:31:22 -0600

    Green intialize and red pack for Hiker

--- spec/hiker_spec.rb
+++ spec/hiker_spec.rb
@@ -7,11 +7,26 @@ RSpec.describe 'Hiker' do
 
       expect(hiker1.class).to eq(Hiker)
     end
-    it 'contains all passed attributes' do
+    it 'contains all starting attributes' do
       hiker1 = Hiker.new('Dora', :moderate)
 
       expect(hiker1.name).to eq('Dora')
       expect(hiker1.experience_level).to eq(:moderate)
+      expect(hiker1.snacks).to eq({})
+    end
+  end
+  describe '#pack' do
+    it 'adds a snack name as a key and snack amount as a value' do
+      hiker1 = Hiker.new('Dora', :moderate)
+
+      hiker1.pack('water', 1)
+      hiker1.pack('trial mix', 3)
+
+      expect(hiker1.snacks).to eq({'water' => 1, 'trail mix' => 3})
+
+      hiker.pack('water', 1)
+
+      expect(hiker1.snacks).to eq({'water' => 2, 'trail mix' => 3})
     end
   end
 end
